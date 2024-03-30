6d20331ff410a7b57af0569fffa94366cc8e61a2
unknown <vladimir_ostrovski@web.de>
2024-03-30 07:32:42 +0100

    work  on lars

--- LARS.R
+++ LARS.R
@@ -1,29 +1,42 @@
 library(glmnet)
 
-df=inSampleSet[[1]]
+LARS.calibrate<-function(df, direction){
+  x=df
+  x$y=NULL
+  x=as.matrix(x)
+  y=df$y
+  
+  # fit LARS model
+  # alpha=1 is the lasso penalty
+  fit = glmnet(x, y, alpha = 1)
+  
+  # perform cross-validation
+  cvfit = cv.glmnet(x, y, alpha = 1) # alpha=1 is the lasso penalty 
 
-x=df
-x$y=NULL
-x=as.matrix(x)
-y=df$y
+  # get the best lambda value
+  bestLambda = cvfit$lambda.min
+  
+  fit$bestLambda=bestLambda
+  return(fit)
+}
 
-# fit LARS model
-# alpha=1 is the lasso penalty
-fit = glmnet(x, y, alpha = 1)
+LARS.getCoef<-function(m){
+  
+  # Get coefficients corresponding to the best lambda
+  finalCoef = predict(m, s = m$bestLambda, type = "coefficients")
+  vFinalCoef= as.matrix(finalCoef)
+  vFinalCoef=as.vector(vFinalCoef)
+  names(vFinalCoef)=finalCoef@Dimnames[[1]]
+  c=vFinalCoef[1]
+  vFinalCoef=vFinalCoef[-1]
+  
+  return(vFinalCoef)
+}
 
-# perform cross-validation
-cvfit = cv.glmnet(x, y, alpha = 1) # alpha=1 is the lasso penalty 
 
-# Plot cross-validation results (optional)
-plot(cvfit)
+LARS.predict<-function(m, outOfSample){
+  res =predict(m, s = m$bestLambda, newx = as.matrix(outOfSample))
+  res=as.vector(res)
+  return(res)
+}
 
-# get the best lambda value
-bestLambda = cvfit$lambda.min
-
-# Get coefficients corresponding to the best lambda
-finalCoef = predict(fit, s = bestLambda, type = "coefficients")
-vFinalCoef= as.matrix(finalCoef)
-vFinalCoef=as.vector(vFinalCoef)
-names(vFinalCoef)=finalCoef@Dimnames[[1]]
-c=vFinalCoef[1]
-vFinalCoef=vFinalCoef[-1]
